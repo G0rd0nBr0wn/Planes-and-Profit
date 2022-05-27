824f7af01db0ec16da8d6424f0690f2ae3724361
civilspeedy <106312897+civilspeedy@users.noreply.github.com>
2022-05-27 18:09:48 +0100

    Add files via upload

--- Planes and Profit/Program.cs
+++ Planes and Profit/Program.cs
@@ -2,10 +2,15 @@
 
 namespace HelloWorld{
     class Program{
-        static void Main(string[] args){
+        static async void Main(string[] args){
             var httpClient= new HttpClient();
-            httpClient.BaseAddress = new Uri("https://yfapi.net/");
 
+            httpClient.BaseAddress = new Uri("https://yfapi.net/");
+            httpClient.DefaultRequestHeaders.Add("X-API-KEY", "<your API key>");
+            httpClient.DefaultRequestHeaders.Add("accept", "application/json");
+
+            var response = await httpClient.GetAsync("v11 / finance / quoteSummary / AAPL ? lang = en & region = US & modules = defaultKeyStatistics % 2CassetProfile");
+            var responseBody = await response.Content.ReadAsStringAsync();
         }
     }
 }
