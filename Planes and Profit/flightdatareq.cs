abd4a49c2532801434def9c95726acedcf7dce65
G0rd0nBr0wn <73000865+G0rd0nBr0wn@users.noreply.github.com>
2022-05-27 18:48:01 +0100

    Add files via upload

---
+++ Planes and Profit/flightdatareq.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using Microsoft.Extensions.Hosting;
+
+
+namespace HelloWorld
+{
+    public class FlightData : BackgroundService
+    {
+        private HttpClient client;
+        private readonly string url = "https://opensky-network.org/api/states/all";
+
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            return base.StartAsync(cancellationToken);  
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            return base.StopAsync(cancellationToken);
+        }
+
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var response = await client.GetAsync(url);
+                Console.WriteLine(response);
+            }
+        }
+
+
+
+    }
+}
