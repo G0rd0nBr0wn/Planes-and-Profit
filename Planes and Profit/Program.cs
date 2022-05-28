0deb7c23ca8dd55a73b275ace74c0fd6b1c90f41
civilspeedy <106312897+civilspeedy@users.noreply.github.com>
2022-05-28 14:13:26 +0100

    Add files via upload

--- Planes and Profit/Program.cs
+++ Planes and Profit/Program.cs
@@ -3,13 +3,29 @@ using Aletheia.Service;
 using Aletheia.Service.StockData;
 
 
-namespace Fetch{
-    class Program{
-        static void Main(string[] args){
-            //only 5,000 calls a month
-            AletheiaService service = new AletheiaService("7FA4842C273E480E82BBDD69723301C4");
-            StockData quote = service.GetStockDataAsync("MSFT", true, true).Result;
-            Console.WriteLine(quote.SummaryData.Price.ToString("#,##0.00"));
+namespace Fetch {
+    class Program {
+        //only 5,000 requests a month
+        static void Main(string[] args) {
+            GetStock("MSFT");
+            
         }
-    }
-}
+        static AletheiaService CallApi(){
+            // Aletheia is called
+            AletheiaService service = new AletheiaService("7FA4842C273E480E82BBDD69723301C4");
+            return service;
+        }
+        static void GetStock(string stockCode) {
+            AletheiaService service = CallApi();
+            try {
+                //request is made
+                StockData quote = service.GetStockDataAsync(stockCode, true, true).Result;
+                Console.WriteLine(quote.SummaryData.Name);
+            }
+            catch (Exception ex) {
+                //the error thrown will be an outside code error
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }   
+}        
